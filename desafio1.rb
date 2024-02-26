e1c4fcaf1099be35aff5f029f2004463beca92bb
Nando Salles <fsclaro@gmail.com>
2024-02-26 16:18:50 -0300

    Desafio 1

---
+++ desafio1.rb
@@ -0,0 +1,10 @@
+print "Entre com seu nome: "
+nome = gets.chomp
+
+print "Entre com seu sobrenome: "
+sobrenome = gets.chomp
+
+print "Entre com sua idade: "
+idade = gets.chomp.to_i
+
+puts "Seu nome é #{nome} #{sobrenome} e você tem #{idade} anos de idade."
